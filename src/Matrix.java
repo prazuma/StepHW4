fb394d9540023518cfed50047307a6df461f4124
mika <example.com>
2015-06-22 23:33:10 +0900

    edit Matrix.java

--- src/Matrix.java
+++ src/Matrix.java
@@ -3,7 +3,19 @@
  */
 public class Matrix {
     public static void main(String[] args){
-
+        if(args.length != 1){
+            System.out.println("usage: java Matrix N");
+            return;
+        }
+        int n = Integer.parseInt(args[0]);
+        double[][] a = initializeMatrixA(n);
+        double[][] b = initializeMatrixB(n);
+        double[][] c = mulMatrix(a, b);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("c[%d][%d]=%f\n", i, j, c[i][j]);
+            }
+        }
     }
     public static double calculateSum(double[][] a){
         if(a == null) return -1;
