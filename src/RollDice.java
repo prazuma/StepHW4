100b993f1e95344cc0f53ad861d0a01345233370
mika <example.com>
2015-06-24 02:16:09 +0900

    サイコロの確率を求めるところをテストに移行

--- src/RollDice.java
+++ src/RollDice.java
@@ -9,32 +9,4 @@ public class RollDice {
         int dice = rand.nextInt(6) + 1;
         return dice;
     }
-
-    public static boolean probabilityDice(int dice){
-        /*
-        numThrow回中1/6回出れば確率は正しい。だが、必ずしもぴったり出るとは限らない。
-        なので、ここでは、誤差の範囲は前後10%まで許し、だいたい1/6回出たらTrueを返すメソッドを作成した。
-        テストでは、1~6がちゃんと1/6で出るかを確かめている。
-        ここでは、人力で設定したが、許容範囲は何か明確な制度？方法？があるならば、
-        それを使用するのが望ましいと思われる。
-        また、サイコロを投げる回数が多くなればなるほど処理の時間がかかる。
-        このプログラムはサイコロを計算するだけなのでさほど気にならないが、
-        時間のかかるプログラムの場合は、時間がかかってしまう可能性がある。
-        */
-        Random rand = new Random();
-        int numThrow = 60000; //投げる回数
-        double tole = 1; //許される誤差(%)
-        double tolerance = numThrow * tole / 100;
-        int count = 0;
-        for(int i = 0; i < numThrow; i++){
-            int n  = rollDice();
-            if(dice == n) {
-                count++;
-            }
-        }
-        double probability = numThrow / 6;
-        if(probability - tolerance <= count && count <= probability + tolerance)
-            return true;
-        return false;
-    }
 }
--- src/RollDiceTest.java
+++ src/RollDiceTest.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.junit.Assert.*;
 
 /**
@@ -9,11 +11,39 @@ public class RollDiceTest {
     
     @Test
     public void testProbabilityDice() throws Exception {
-        assertTrue(RollDice.probabilityDice(1));
-        assertTrue(RollDice.probabilityDice(2));
-        assertTrue(RollDice.probabilityDice(3));
-        assertTrue(RollDice.probabilityDice(4));
-        assertTrue(RollDice.probabilityDice(5));
-        assertTrue(RollDice.probabilityDice(6));
+        assertTrue(probabilityDice(1));
+        assertTrue(probabilityDice(2));
+        assertTrue(probabilityDice(3));
+        assertTrue(probabilityDice(4));
+        assertTrue(probabilityDice(5));
+        assertTrue(probabilityDice(6));
+    }
+
+    public static boolean probabilityDice(int dice){
+        /*
+        numThrow回中1/6回出れば確率は正しい。だが、必ずしもぴったり出るとは限らない。
+        なので、ここでは、誤差の範囲は前後10%まで許し、だいたい1/6回出たらTrueを返すメソッドを作成した。
+        テストでは、1~6がちゃんと1/6で出るかを確かめている。
+        ここでは、人力で設定したが、許容範囲は何か明確な制度？方法？があるならば、
+        それを使用するのが望ましいと思われる。
+        また、サイコロを投げる回数が多くなればなるほど処理の時間がかかる。
+        このプログラムはサイコロを計算するだけなのでさほど気にならないが、
+        時間のかかるプログラムの場合は、時間がかかってしまう可能性がある。
+        */
+        Random rand = new Random();
+        int numThrow = 60000; //投げる回数
+        double tole = 1; //許される誤差(%)
+        double tolerance = numThrow * tole / 100;
+        int count = 0;
+        for(int i = 0; i < numThrow; i++){
+            int n  = RollDice.rollDice();
+            if(dice == n) {
+                count++;
+            }
+        }
+        double probability = numThrow / 6;
+        if(probability - tolerance <= count && count <= probability + tolerance)
+            return true;
+        return false;
     }
 }
